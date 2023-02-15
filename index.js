2f84e54e1f97d0c26b53531d0c7b60fd21f6468d
Veronica Harris <s2vlharr@hotmail.com>
2023-02-15 17:48:33 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+//import the http
+const http = require("http")
+
+//create a server with HTTP
+const server = http.createServer((req, res)=> {
+    console.log("the server is created")
+})
+
+//initiate a port
+const PORT = 4000;
+
+//listen to the server
+server.listen(PORT, ()=> console.log(`The server is running on port ${PORT}`))
